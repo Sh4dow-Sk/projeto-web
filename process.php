17e770c8f6fd3f9b050211a9e6fdea4d6964ab0f
Fc <56210099+Sh4dow-Sk@users.noreply.github.com>
2022-11-10 01:13:23 -0300

    Add files via upload

--- process.php
+++ process.php
@@ -1,4 +1,18 @@
 <?php
-    $login = $_POST['login'];
-    $senha = $_POST['senha'];
+	$usuario = $_POST['login'];
+	$senha = $_POST['senha'];
+	
+	if ($usuario=="admin" && $senha=="admin") {
+		if (session_status() !== PHP_SESSION_ACTIVE) {
+			session_start();
+		}
+
+		$_SESSION['usuario'] = 'admin';
+		header('Location: /painel.html');
+
+	}
+	else {
+		echo "ERRO!";
+	}
+
 ?>
